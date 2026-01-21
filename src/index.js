0c2452ed6a10f170b21e2534a9ee34f758b17112
luizfelipe17 <luizfeliperabelo17@gmail.com>
2026-01-21 19:03:35 -0300

    google

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';  
 
 
